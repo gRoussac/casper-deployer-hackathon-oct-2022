9e5ef87f6fc74449a8e2de3760b0f4fe23053582
gRoussac <gRoussac@users.noreply.github.com>
2022-10-19 03:01:23 +0200

    server missing

---
+++ www/dist/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const proxy = require('http-proxy-middleware');
+const app = express();
+const port = process.env.PORT || 8080;
+
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/apps/escrow/index.html'));
+});
+
+app.use('/', express.static(path.join(__dirname, 'apps/escrow/')));
+
+const apiProxy = proxy.createProxyMiddleware('/api', { target: 'http://localhost:3333' });
+app.get('/api/*', apiProxy);
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);

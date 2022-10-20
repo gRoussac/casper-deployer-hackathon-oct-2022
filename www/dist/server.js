4817e3d93f6cd92ad84cbb4f5e0b0b11989221d7
Gregory Roussac <gRoussac@users.noreply.github.com>
2022-10-20 18:42:57 +0200

    Render Router (#13)

--- www/dist/server.js
+++ www/dist/server.js
@@ -4,6 +4,11 @@ const proxy = require('http-proxy-middleware');
 const app = express();
 const port = process.env.PORT || 8080;
 
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/apps/frontend/index.html'));
+});
+
 app.use('/', express.static(path.join(__dirname, 'apps/frontend/')));
 
 const apiProxy = proxy.createProxyMiddleware('/api', { target: 'http://localhost:3333' });

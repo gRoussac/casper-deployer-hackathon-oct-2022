11defec21d35b1e8978399afef4adc1b36f96d35
Gregory Roussac <gRoussac@users.noreply.github.com>
2022-10-20 18:02:49 +0200

    Add Greeter (#11) (#12)
    * Add Greeter
    
    * Fix inset!

--- www/dist/server.js
+++ www/dist/server.js
@@ -5,9 +5,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // sendFile will go here
-// app.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname, '/apps/frontend/index.html'));
-// });
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/apps/frontend/index.html'));
+});
 
 app.use('/', express.static(path.join(__dirname, 'apps/frontend/')));
 

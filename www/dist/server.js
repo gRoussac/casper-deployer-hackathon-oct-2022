a64af0a87c2ac44e2ab6379ac81a18e0c693d5ce
gRoussac <gRoussac@users.noreply.github.com>
2022-10-20 16:56:47 +0200

    Deploy fails

--- www/dist/server.js
+++ www/dist/server.js
@@ -5,11 +5,11 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // sendFile will go here
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/apps/frontend/index.html'));
-});
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname, '/apps/frontend/index.html'));
+// });
 
-app.use('/', express.static(path.join(__dirname, 'apps/escrow/')));
+app.use('/', express.static(path.join(__dirname, 'apps/frontend/')));
 
 const apiProxy = proxy.createProxyMiddleware('/api', { target: 'http://localhost:3333' });
 app.get('/api/*', apiProxy);

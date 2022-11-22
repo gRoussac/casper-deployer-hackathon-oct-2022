fb86aed5923913537eead8da11304ae7259bb26c
gRoussac <gRoussac@users.noreply.github.com>
2022-11-23 00:07:04 +0100

    Add 50 mb api limit

--- www/apps/api/src/main.ts
+++ www/apps/api/src/main.ts
@@ -5,13 +5,15 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { urlencoded, json } from 'express';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   const port = 3333;
   await app.listen(port);
   Logger.log(

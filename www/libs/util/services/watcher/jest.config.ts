ffff8cbb7aba437618c6e1ab3c78e46a5c59063b
gRoussac <gRoussac@users.noreply.github.com>
2024-01-07 02:19:57 +0100

    Fix jest run

--- www/libs/util/services/watcher/jest.config.ts
+++ www/libs/util/services/watcher/jest.config.ts
@@ -1,17 +1,16 @@
-/* eslint-disable */
 export default {
   displayName: 'util-services-watcher',
   preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
   coverageDirectory: '../../../../coverage/libs/util/services/watcher',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
